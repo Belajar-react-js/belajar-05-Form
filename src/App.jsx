c3dfe6aa3cb4cfdaf6a1f79d1147e42e64649872
kharisma tara mukti <kharismataram@gmail.com>
2023-05-16 18:04:57 +0700

    form

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import NavbarSipenting from "./Componen/Navbar";
+import Footer from "./Componen/Footer";
+import Register from "./Componen/Register";
+import RegisterRefactor from "./Componen/RegisterRefactor";
+
+function App() {
+  return (
+    <>
+      <NavbarSipenting />
+
+      <Register />
+
+      <br />
+      <br />
+      <RegisterRefactor />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Componen/Navbar.jsx
@@ -0,0 +1,33 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+
+function NavbarSipenting() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container fluid>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="justify-content-end flex-grow-1 pe-3">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            <Button variant="outline-success me-2">Search</Button>
+            <Button variant="outline-success">Search</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarSipenting;
